bfb7382b5210c3edd5d3a0399f8ec2ab58f4e8d4
Roberto Delgado <robertodelgado@Robertos-MacBook-Air.local>
2020-01-19 16:52:33 -1000

    Cleared newTask after input

--- src/components/TodoComponents/TodoForm/TodoForm.js
+++ src/components/TodoComponents/TodoForm/TodoForm.js
@@ -19,6 +19,7 @@ class TodoForm extends React.Component {
   handleSubmit = e => {
     e.preventDefault();
     this.props.addNewTask(this.state.newTask);
+    this.setState({newTask: ""});
   };
 
   render() {
